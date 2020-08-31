1e76de0a4f396b4886724d0b20462d0c868ec46f
Maria Souza <messenger.mariah2017@gmail.com>
2020-08-31 11:46:51 -0300

    Atribuiçao de variaveis, const, let e maneiras de cocatenação

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Atribuição de Variáveis");
+
+
+
+
+const idade = 26;
+const primeiroNome = "Maria";
+const sobrenome = "Souza";
+
+ //Maneiras de cocatenação
+
+//console.log(nome , sobrenome);
+console.log(primeiroNome + " " + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome} e tenho ${idade} anos.`)
+
+const nomeCompleto = primeiroNome + sobrenome; //sobrescrevendo
+
+
+console.log(nomeCompleto);
