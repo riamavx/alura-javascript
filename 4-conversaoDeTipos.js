5658c6f400ef79a80f48784b4db10e256740b873
Maria Souza <messenger.mariah2017@gmail.com>
2020-08-31 11:25:00 -0300

    conversão de tipos, uso do parseInt e diferença de usos com . e ,

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,15 @@
+console.log("Conversão de Tipos");
+
+
+
+console.log("ano" + " ", 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));  //conversão explicita
+
+console.log("20" / "10");  //conversão implicita
+
+
+console.log("Maria" / "2");  // retorna um Nan (not a number)
+
+console.log(10.5);
+console.log(10,5); //serve para funções especiais 
