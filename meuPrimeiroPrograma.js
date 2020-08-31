70eb6cf480eb8a08e5e3a33171cf593f3d7dbde9
Maria Souza <messenger.mariah2017@gmail.com>
2020-08-31 10:38:33 -0300

    imprimindo valores no node.js/ operações básicas

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,16 @@
+console.log("Meu Primeiro Programa: Trabalhando com Variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
+
+// const nome = "maria";
+// console.log("nome")
