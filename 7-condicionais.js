b1441fcd0a397088fa8c53a85b83b8e8ff499bd4
Maria Souza <messenger.mariah2017@gmail.com>
2020-09-03 10:59:41 -0300

    operadores logicos

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,24 +8,28 @@ const listasDeDestinos = new Array(
 
 );
 
-const idadeComprador = 10;
-const estaAcompanhada = true;
+const idadeComprador = 32;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
+
 console.log("Destinos Possíveis:");
 console.log(listasDeDestinos);
 
 
-if (idadeComprador >= 18) {
+if (idadeComprador >= 18 || estaAcompanhada == true ) {
     console.log("Comprador maior de Idade");
     listasDeDestinos.splice(1, 1); // removendo um item
-} else if (estaAcompanhada == true) {
-    console.log("Comprador esta acompanhado");
-    listasDeDestinos.splice(1, 1); // removendo um item
-
 } else {
     console.log("Não é maior de idade, não posso vender")
 }
 
+console.log("Embarque:\n\n");
 
+if(idadeComprador > 18 && temPassagemComprada){
+    console.log("Boa Viagem")
+}else{
+    console.log("Sorry, você não pode embarcar!")
+}
 
 console.log(listasDeDestinos);
 
