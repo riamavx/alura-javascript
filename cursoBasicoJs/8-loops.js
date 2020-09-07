0a5c3a0494864191125c15d9b14ccfc99224b72c
Maria Souza <messenger.mariah2017@gmail.com>
2020-09-07 10:33:43 -0300

    Oraganizaçãod de pastas e inicio do curso intro a orientação obj

--- cursoBasicoJs/8-loops.js
+++ cursoBasicoJs/8-loops.js
@@ -40,7 +40,7 @@ console.log("Destino existe", destinoExiste)
 
 
 
-for(let i = 0; i <3; i++){
+for(let i = 0; i <4; i++){
     if(listasDeDestinos[i] == destino){
         destinoExiste = true
         
