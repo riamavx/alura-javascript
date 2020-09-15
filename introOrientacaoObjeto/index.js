c8849022365386ec009afd5185561b85463df349
Maria Souza <messenger.mariah2017@gmail.com>
2020-09-15 18:06:39 -0300

    atributos privados

--- introOrientacaoObjeto/index.js
+++ introOrientacaoObjeto/index.js
@@ -1,38 +1,52 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
+    
 }
 
+
+class ContaCorrente{
+    agencia;
+    #saldo = 0;
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+        }
+    }
+
+
+    depositar(valor){
+        if(valor > 0){
+            this.#saldo += valor;
+        }
+    }
+}
+
+
+
 const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+
+
 const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
- cliente1.nome = "Ricardo";
- cliente1.cpf = 11122233309;
- cliente1.agencia = 1001;
- cliente1.saldo = 0;
-
-
-
- 
-
  cliente2.nome = "Alice";
  cliente2.cpf = 88822233309;
- cliente2.agencia = 1001;
- cliente2.saldo = 0;
+ 
 
+
+ const contaCorrenteRicardo = new ContaCorrente();
+ 
+ contaCorrenteRicardo.agencia = 1001; 
 
  
 
- cliente3.nome = "Maria";
- cliente3.cpf = 99922233309;
- cliente3.agencia = 1001;
- cliente3.saldo = 0;
+contaCorrenteRicardo.depositar ( 100);
+contaCorrenteRicardo.sacar(50)
 
 
 
 
-console.log(cliente1, cliente2, cliente3)
+console.log(contaCorrenteRicardo)
